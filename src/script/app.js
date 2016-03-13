56a95849cac0e85233c938ccbf9afc36ae55c8d6
metaraine <raineorshine@gmail.com>
2016-03-13 10:50:54 -0600

    Add RefactorU content. Tweak styles. Add ncu stars. Resize images.

--- src/script/app.js
+++ src/script/app.js
@@ -1,17 +1,20 @@
 var $ = require('jquery')
 var template20 = require('template20')
 var prop = require('lodash.property')
+var flow = require('lodash.flow')
+var addCommas = require('add-commas')
 
 var repos = [
   'binpath',
   'cint',
   'creatable',
   'dotfiles',
+  'emitter20',
   'freebusy',
-  'generator-yoga',
   'get-stdin-promise',
   'gulp-striate',
   'nativity',
+  'plural-parens',
   'prefixnote',
   'promise-guard',
   'shackles',
@@ -24,15 +27,27 @@ var repos = [
   'template20',
   'whipdb',
   'wordsoap',
-  'x-to-y'
+  'x-to-y',
+  'zones'
 ]
+
+// add a period to a sentence if it doesn't have one
+function endSentence(str) {
+  return str + (str[str.length-1] !== '.' ? '.' : '')
+}
 
 $(function () {
 
-  // populate
+  var starsEl = $('#ncu-stars')
+  var setStars = starsEl.text.bind(starsEl)
+
+  // get number of stars of npm-check-updates
   $.get('https://api.github.com/repos/tjunnone/npm-check-updates')
-    .then(prop('stargazers_count'))
-    .then(console.log.bind(console))
+    .then(flow(
+      prop('stargazers_count'),
+      addCommas,
+      setStars
+    ))
 
   // populate other repos
   repos.forEach(function (reponame) {
@@ -42,7 +57,7 @@ $(function () {
       name: reponame,
       url: 'https://github.com/metaraine/' + reponame,
       description: $.get('https://api.github.com/repos/metaraine/' + reponame)
-        .then(prop('description'))
+        .then(flow(prop('description'), endSentence))
     })
 
     // add the new row to the table
